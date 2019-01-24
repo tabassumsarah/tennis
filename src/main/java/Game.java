26f319fdb5bb13d6c8076b281737acc1da911b41
s111591 <sarah.x.tabassum@iag.com.au>
2019-01-24 11:33:00 +1100

    Tennis-Task: -Added score list to pass the test

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,9 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by saraht on 24/01/2019.
  */
 public class Game {
     String player1;
     String player2;
+
+    //need some score variable
+    List<String> score = Arrays.asList("0","0");
 
     public Game(String player1, String player2) {
         this.player1 = player1;
@@ -15,7 +21,8 @@ public class Game {
     }
 
     String score(){
-        return "0-0, 15-15";
+        String scoreResult = score.get(0)+"-"+ score.get(1);
+        return "0-0, " + scoreResult;
     }
 
 
