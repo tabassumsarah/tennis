304da89b95ebb2c8cdc0a8dbec706adce20416e2
s111591 <sarah.x.tabassum@iag.com.au>
2019-01-24 11:12:20 +1100

    Tennis-Task: Added first test

---
+++ src/test/java/TennisGameTests.java
@@ -0,0 +1,10 @@
+/**
+ * Created by saraht on 24/01/2019.
+ */
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TennisGameTests extends TestCase{
+
+}
