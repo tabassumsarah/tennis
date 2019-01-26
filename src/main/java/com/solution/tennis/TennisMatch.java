9cb101f1b70e3e2654608327a199ecf7bd75cde8
s111591 <sarah.x.tabassum@iag.com.au>
2019-01-26 11:16:25 +1100

    Tennis Game : Added a test class and Match class

---
+++ src/main/java/com/solution/tennis/TennisMatch.java
@@ -0,0 +1,7 @@
+package com.solution.tennis;
+
+/**
+ * Created by saraht on 26/01/2019.
+ */
+public class TennisMatch {
+}
---
+++ src/test/java/com/solution/tennis/TennisMatchTests.java
@@ -0,0 +1,11 @@
+package com.solution.tennis;
+
+import org.junit.Test;
+
+/**
+ * Created by saraht on 26/01/2019.
+ */
+
+public class TennisMatchTests {
+
+}
